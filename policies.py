e0e401ebf50c3c8d28f216f55cfc630fe83dfe0a
hataloo <jacksandberg998@gmail.com>
2021-11-28 13:47:36 +0100

    Added CustomVisionNet, ppo_example and worked on CustomNetwork.

--- policies.py
+++ policies.py
@@ -2,16 +2,47 @@ from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from torch import nn
 import gym
 from ray.rllib.utils.typing import ModelConfigDict
-
+from typing import Dict, Any, List
+import torch
 class CustomNetwork(TorchModelV2, nn.Module):
     """Custom CNN Network"""
 
     def __init__(self, obs_space: gym.spaces.Space, action_space: gym.spaces.Space, num_outputs: int, model_config: ModelConfigDict, name: str):
-        TorchModelV2.__init__(obs_space, action_space, num_outputs, model_config, name)
+        TorchModelV2.__init__(self, obs_space, action_space, num_outputs, model_config, name)
+        
         nn.Module.__init__(self)
 
-        in_channels = obs_space.shape[0]
-
-        self.sequentlial_layers = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=(5, 5), stride=1)
+        in_channels = obs_space.shape[-1]
+        #for i in range(1000):
+        #    print(obs_space.shape)
+        self.sequential_layers = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=16, kernel_size=(5, 5), stride=1, padding = 'same'),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(4, 4), stride=1, padding = 'same'),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(7200, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
         )
+        self.output_layer = nn.Linear(512,num_outputs)
+        self.value_layer = nn.Linear(512,1)
+        self._value = None
+        
+    def forward(self, input_dict, state, seq_lens):
+        obs = input_dict["obs"].detach().clone().float()
+        obs = torch.permute(obs, (0, 3, 1, 2))
+        #for i in range(1000):
+        #    print(obs.shape)
+        x = self.sequential_layers(obs)
+        #model_out, self._value_out = self.base_model(
+        #    input_dict["obs"])
+        self._value = self.value_layer(x)
+        return self.output_layer(x), state
+
+    def value_function(self):
+        assert self._value is not None, "must call forward() first"
+        #for i in range(50):
+        #    print(self._value.shape)
+        return self._value.squeeze(1)
---
+++ train_ppo.py
@@ -0,0 +1,96 @@
+import ray
+import ray.rllib.agents.ppo as ppo
+from ray.tune.logger import pretty_print
+#from ray.rllib.agents.dqn import DQNTrainer, DQNTorchPolicy
+from ray.rllib.agents.ppo import PPOTrainer, PPOTorchPolicy
+
+from battlesnake_gym import BattlesnakeGym
+from battlesnake_gym.rewards import SimpleRewards
+
+from CustomVisionNet import VisionNetworkWithPooling
+from policies import CustomNetwork
+from ray.rllib.models import ModelCatalog
+ModelCatalog.register_custom_model("VisionNetworkWithPooling", VisionNetworkWithPooling)
+ModelCatalog.register_custom_model("CustomNetwork", CustomNetwork)
+
+
+def policy_mapping_fn(agent_id, *arg):
+    return "single_snake"
+
+dummy_env = BattlesnakeGym()
+obs_space = dummy_env.observation_space
+act_space = dummy_env.action_space
+
+ray.init()
+config = ppo.DEFAULT_CONFIG.copy()
+
+# trainer settings
+config["num_gpus"] = 1
+config["num_workers"] = 1
+config["num_envs_per_worker"] = 1
+# carefull this can render a lot of videos
+config["record_env"] = False
+# env configs
+config["env_config"] = {
+                "observation_type": "flat-51s",
+                "map_size": (16, 16),
+                "number_of_snakes": 4, 
+                "snake_spawn_locations": [],
+                "food_spawn_locations": [],
+                "verbose": False,
+                "initial_game_state": None,
+                "rewards": SimpleRewards()
+            }
+config["framework"] = "torch"
+# model settings
+
+config["model"] = {
+    #"custom_model" : "VisionNetworkWithPooling",
+    "custom_model" : "CustomNetwork",
+    "dim": 16,
+    "no_final_linear": False,
+    "vf_share_layers": False,
+    "conv_activation": "relu",
+    "conv_filters": [[16, [5,5], 1], [32, [4,4], 1]],
+    "post_fcnet_hiddens": [513, 514],
+    "post_fcnet_activation": "relu",
+    "custom_model_config": {
+    "post_fcnet_inputsize" : 512} #The size of the output after convolutions, calculate by hand.
+}
+
+# multi agent settings
+config["multiagent"] = {
+    "policies": {
+        "single_snake": (PPOTorchPolicy, obs_space, act_space, {})
+    },
+    "policy_mapping_fn": policy_mapping_fn
+}
+# set evaluation options
+config["evaluation_interval"] = 3000
+
+
+trainer = PPOTrainer(config=config, env=BattlesnakeGym)
+
+# Can optionally call trainer.restore(path) to load a checkpoint.
+
+for i in range(1000):
+   # Perform one iteration of training the policy with PPO
+   result = trainer.train()
+   print(pretty_print(result))
+
+   if i % 100 == 0:
+       checkpoint = trainer.save()
+       print("checkpoint saved at", checkpoint)
+
+# Also, in case you have trained a model outside of ray/RLlib and have created
+# an h5-file with weight values in it, e.g.
+# my_keras_model_trained_outside_rllib.save_weights("model.h5")
+# (see: https://keras.io/models/about-keras-models/)
+
+# ... you can load the h5-weights into your Trainer's Policy's ModelV2
+# (tf or torch) by doing:
+# trainer.import_model("my_weights.h5")
+# NOTE: In order for this to work, your (custom) model needs to implement
+# the `import_from_h5` method.
+# See https://github.com/ray-project/ray/blob/master/rllib/tests/test_model_imports.py
+# for detailed examples for tf- and torch trainers/models.
